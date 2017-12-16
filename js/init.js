5e4066213e8c28e15ec75189500e59c6d7fb25c9
Ben Burdess <benburdess@yahoo.com>
2017-12-16 13:13:08 -0600

    commit #2
    Bug fixes and things like that

---
+++ js/init.js
@@ -0,0 +1,29 @@
+
+
+    (function($){
+        $(function(){
+      
+          $('.parallax').parallax();
+      
+        }); // end of document ready
+      })(jQuery); // end of jQuery name space
+      
+       $(document).ready(function(){
+          $('.tooltipped').tooltip({delay: 50});
+        });
+              $(document).ready(function(){
+          $('.tooltipped').tooltip({delay: 50});
+        });
+
+        $( document ).ready(function(){
+            $(".button-collapse").sideNav();
+        
+            $(document).ready(function(){
+              $('.slider').slider();
+            });
+                
+        
+          $(document).ready(function(){
+            $('.slider').slider();
+          });
+               
